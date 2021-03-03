6549577dda66a75e553fd3bc769877a36ded55b4
ROJAS MARTINEZ <s16120309@alumnos.itsur.edu.mx>
2021-03-03 12:30:51 -0600

    Creación de clase héroe y vista del recyclerview

---
+++ app/src/main/java/net/ivanvega/peticioneshttpconvolley/Heroe.java
@@ -0,0 +1,27 @@
+package net.ivanvega.peticioneshttpconvolley;
+
+public class Heroe {
+    String name;
+    String imageurl;
+
+    public Heroe(String name, String imageurl) {
+        this.name = name;
+        this.imageurl = imageurl;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImageurl() {
+        return imageurl;
+    }
+
+    public void setImageurl(String imageurl) {
+        this.imageurl = imageurl;
+    }
+}
